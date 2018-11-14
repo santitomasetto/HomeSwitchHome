be27adb6b5459b1c8616a809f667cca467e9125a
santitomasetto <tomasetto@tomasetto.tomasetto>
2018-11-13 23:30:23 -0300

    editar subastas y puja

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -18,18 +18,25 @@ class AuctionsController < ApplicationController
   end
 
   def update
+    @auction = Auction.find(params[:id])
+    if @auction.update(params.require(:auction).permit(:name,:amount,:in_date,:out_date,:bid,:residence_id))
+      redirect_to auctions_path
+    else
+      render :edit
+    end
   end
 
   def destroy
   	auction= Auction.find(params[:id])
   	if auction.destroy
-  		redirect_to auctions_path, notice: "la subasta #{auction.name} ha sido eliminada"
+  		redirect_to auctions_path, notice: "la subasta '#{auction.name}' ha sido eliminada"
   	else
-  		redirect_to auctions_path, notice: "ERROR al eliminar la subasta #{auction.name}"
+  		redirect_to auctions_path, notice: "ERROR al eliminar la subasta '#{auction.name}'"
   	end
   end
 
   def edit
+    @auction=Auction.find(params[:id])
   end
 
   def bid_up
@@ -42,7 +49,7 @@ class AuctionsController < ApplicationController
     if @auction.save 
       redirect_to auctions_path, notice: "La subasta se creo exitosamente"
     else  
-      redirect_to auctions_path, notice: "ERROR al crear la subasta"
+      redirect_to :new
     end
     
   end
