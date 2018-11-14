6d47586e547ab413a9f476e6ee9138d593fd4d9a
Lahus <mauu.bob@hotmail.com>
2018-11-14 18:57:31 -0300

    Se soluciono problema de residencias iguales

--- app/controllers/residences_controller.rb
+++ app/controllers/residences_controller.rb
@@ -17,25 +17,21 @@ class ResidencesController < ApplicationController
   def update
     @residence=Residence.find(params[:id])
 
-    if @residence.present?
-      flash[:notice] = 'La residencia ya existe'
-      render :edit
-    else
       if @residence.update(params.require(:residence).permit(:name,:country,:address,:capacity,:photo))
         redirect_to residences_path
       else
+        flash[:notice] = "La residencia ya existe"
         render :edit
       end
-    end
   end
 
   def destroy
     residence=Residence.find(params[:id])
 
     if residence.destroy
-      redirect_to residences_path, notice: "La Residencia '#{residence.name}' ha sido eliminada"
+      redirect_to residences_path, success: "La Residencia '#{residence.name}' ha sido eliminada"
     else
-      redirect_to residences_path, notice: "Error al eliminar la Residencia '#{residence.name}'"
+      redirect_to residences_path, success: "Error al eliminar la Residencia '#{residence.name}'"
     end
   end
 
@@ -46,15 +42,12 @@ class ResidencesController < ApplicationController
   def create
     @residence=Residence.new(params.require(:residence).permit(:name,:country,:address,:capacity,:photo))
 
-    if @residence.present?
-      flash[:notice] = 'La residencia ya existe'
-      render :new
-    else
       if @residence.save
         redirect_to residences_path
       else
+        flash[:notice] = "La residencia ya existe"
         render :new
       end
-    end
+    
   end
 end
--- app/models/residence.rb
+++ app/models/residence.rb
@@ -1,3 +1,4 @@
 class Residence < ApplicationRecord
 	has_many :auctions, dependent: :destroy
+	validates :name, :uniqueness => {:scope => [:country, :address, :capacity, :photo,]}
 end
