8b51065d9ce87e1ce218afa6a06a92413f421923
santitomasetto <tomasetto@tomasetto.tomasetto>
2018-11-22 17:32:57 -0300

    agregado columna premium

---
+++ db/migrate/20181122202920_agregar_premium_user.rb
@@ -0,0 +1,5 @@
+class AgregarPremiumUser < ActiveRecord::Migration[5.2]
+  def change
+  	add_column :users, :premium, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_201007) do
+ActiveRecord::Schema.define(version: 2018_11_22_202920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2018_11_22_201007) do
     t.date "card_vto"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "premium"
   end
 
 end
