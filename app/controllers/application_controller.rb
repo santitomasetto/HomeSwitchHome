d2d91ce3ba0af1bc578367f9d2eaf58341bab6a2
Lahus <mauu.bob@hotmail.com>
2018-11-27 12:49:19 -0300

    se guardan todos los parametros ahora y hace diferencia entre admin y usuario comun

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+	#protect_from_forgery with :exception
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+		def configure_permitted_parameters
+			devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :birthdate, :password, :card_number, :card_security_number, :cart_vto, :premium, :admin])
+			devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :birthdate, :password, :current_password, :card_number, :card_security_number, :cart_vto, :premium, :admin])
+		end
 end
