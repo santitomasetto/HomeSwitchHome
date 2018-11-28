e1e3141ccd55a5368276e7896fe6cde57d71caa5
santitomasetto <tomasetto@tomasetto.tomasetto>
2018-11-28 05:27:10 -0300

    premium y navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
   def index
   	@users = User.all
   end
+
+  def make_premium
+  	@user=User.find(params[:id])
+  	@user.premium=true
+  	if @user.save
+  		flash.notice = "El usuario ya es premium"
+  		redirect_to users_index_path
+  	else
+  		flash.alarm = "ERROR al hacer usuario premium"
+  		redirect_to users_index_path
+  	end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,13 @@ Rails.application.routes.draw do
   get '/contacto', to: 'contacts#show'
   get '/terminosycondiciones', to: 'conditions#show'
 
-  #devise_for :users
   devise_for :users, controllers: {
         sessions: 'users/sessions',
         passwords: 'users/passwords',
         registrations: 'users/registrations'
       }
+  get '/users/index', to: 'users#index'
+  get '/users/make_premium', to: 'users#make_premium'
 
 
   root 'residences#index'
