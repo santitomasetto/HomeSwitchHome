be446139e6f7ebfd26b7176864f3c882da4d893c
santitomasetto <tomasetto@tomasetto.tomasetto>
2018-12-05 15:10:41 -0300

    puja y alquiler corregidos

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -30,27 +30,38 @@ class ReservationsController < ApplicationController
 
   def create
     @reservation= Reservation.new(params.require(:reservation).permit(:in_date, :user_id, :residence_id))
-    if Reservation.where(user_id: @reservation.user_id).count < 2
-   	  if @reservation.in_date.wday == 1 
-          if (@reservation.in_date.month - Time.now.month) + 12 * (@reservation.in_date.year - Time.now.year) >= 6
-            if @reservation.save 
-              flash.notice = "La reservacion se realizo exitosamente"
-              redirect_to root_path
-            else  
-              flash.alert = "La fecha ya esta reservada"
-              redirect_to root_path
-            end
-          else
-            flash.now[:alert] = 'Se debe crear con 6 meses de anticipacion'
-            render :new
-          end     
-      else
-        flash.now[:alert] = 'La subasta deeb comenzar un lunes'
-        render :new
-      end
-    else
-    	flash.now[:alert] = 'Exediste la cantidad de semanas disponibles'
-    	render :new
+    @cumple=true
+    @reservation.residence.auctions.each do |a|
+    	if a.in_date == @reservation.in_date
+    		@cumple=false
+    	end
     end
+    if @cumple
+    	if Reservation.where(user_id: @reservation.user_id).count < 2
+   		  if @reservation.in_date.wday == 1 
+    	      if (@reservation.in_date.month - Time.now.month) + 12 * (@reservation.in_date.year - Time.now.year) >= 6
+    	        if @reservation.save 
+    	          flash.notice = "La reservacion se realizo exitosamente"
+    	          redirect_to root_path
+    	        else  
+    	          flash.alert = "La fecha ya esta reservada"
+    	          redirect_to root_path
+    	        end
+    	      else
+    	        flash.now[:alert] = 'Se debe crear con 6 meses de anticipacion'
+    	        render :new
+    	      end     
+    	  else
+    	    flash.now[:alert] = 'La subasta deeb comenzar un lunes'
+    	    render :new
+    	  end
+    	else
+    		flash.now[:alert] = 'Exediste la cantidad de semanas disponibles'
+    		render :new
+    	end
+    else
+    	flash.now[:alert] = 'La residencia se encuentra en subasta esa semana'
+    	render :new
+  	end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_054918) do
+ActiveRecord::Schema.define(version: 2018_12_01_000947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2018_11_28_054918) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "residence_id"
+  end
+
+  create_table "hotsales", force: :cascade do |t|
+    t.date "in_date"
+    t.float "amount"
+    t.integer "residence_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reservations", force: :cascade do |t|
