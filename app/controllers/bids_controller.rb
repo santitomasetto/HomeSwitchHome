e3cb17d600497996c77d7c525a569b20b3a53f9f
santitomasetto <tomasetto@tomasetto.tomasetto>
2018-12-11 18:07:26 -0300

    no volver a pujar si ganas

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -8,11 +8,16 @@ class BidsController < ApplicationController
 
 	def create
 		@bid=Bid.new(params.require(:bid).permit(:amount, :user_id, :auction_id))
-		if @bid.save
-			flash.notice="La puja se registro exitosamente"
-			redirect_to auctions_path
+		if @bid.auction.bids.order(amount: :desc).first.user_id != @bid.user_id
+			if @bid.save
+				flash.notice = "La puja se registro exitosamente"
+				redirect_to auctions_path
+			else
+				flash.alert = "ERROR al registrar la puja"
+				render :new
+			end
 		else
-			flash.alert("ERROR al registrar la puja")
+			flash.alert = "Usted posee la puja mas alta"
 			render :new
 		end
 	end
